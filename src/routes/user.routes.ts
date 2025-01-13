2f451fe031ab77e6663b10bee7f1d521780c965d
Karen Cinca <karencinca1@gmail.com>
2025-01-12 21:03:32 -0300

    creating and updating users with hashed password

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,5 +4,6 @@ import { UserController } from "../controllers/user.controller";
 const userController = new UserController()
 
 export async function user(app: FastifyInstance) {
-    app.post("/signup", userController.createUser)
+    app.post("/signup", userController.create)
+    app.put("/editprofile", userController.update)
 }
--- src/usecases/user.usercases.ts
+++ src/usecases/user.usercases.ts
@@ -2,9 +2,16 @@ import { prisma } from "../lib/prisma";
 import argon2 from "argon2"
 
 export interface UserSignupRequest {
-    name: string
-    email: string
+    name: string,
+    email: string,
     password: string
+}
+
+export interface UserEditRequest {
+    name?: string,
+    email?: string,
+    password?: string,
+    old_password?: string
 }
 
 export const signup = async (data: UserSignupRequest) => {
@@ -30,10 +37,49 @@ export const signup = async (data: UserSignupRequest) => {
     return user
 }
 
-export const updateUser = async (data: UserSignupRequest) => {
-    // const user = await prisma.user.findFirst({
-    //     where: {
+export const edit = async (id: string, data: UserEditRequest) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            id
+        }
+    })
+    if (!user) {
+        throw new Error("Usuário não encontrado.")
+    }
 
-    //     }
-    // })
+    const userWithUpdatedEmail = await prisma.user.findFirst({
+        where: {
+            email: user.email
+        }
+    })
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+        throw new Error("E-mail já cadastrado.")
+    }
+
+    if (data.password && !data.old_password) {
+        throw new Error("Informar a senha antiga.")
+    }
+
+    if (data.password && data.old_password) {
+        const checkOldPassword = await argon2.verify(user.password, data.old_password)
+
+        if(checkOldPassword) {
+            user.password = await argon2.hash(data.password)
+        } else {
+            throw new Error("A senha antiga não confere.")
+        }
+    }
+
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: user.id
+        },
+        data: {
+            name: data?.name || user.name,
+            email: data?.email || user.email,
+            password: user?.password
+        }
+    })
+    return updatedUser
 }
