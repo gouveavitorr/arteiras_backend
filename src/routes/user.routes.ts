e96c88e78b11740d51accd6598cabbf22882dc59
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-05-22 13:25:00 -0300

    fix: re-enable auth middleware in customer route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,7 +8,7 @@ export async function user(app: FastifyInstance) {
     app.post("/signup", userController.create)
     app.put("/editprofile", { preHandler: isAuthenticated }, userController.update)
     app.post("/login", userController.login)
-    app.get("/customer", /*{ preHandler: isAuthenticated },*/ userController.getProfile)
+    app.get("/customer", { preHandler: isAuthenticated }, userController.getProfile)
     app.put("/customer", { preHandler: isAuthenticated }, userController.updateProfile)
     app.get("/user/orders", { preHandler: isAuthenticated }, userController.getOrders)
 }
