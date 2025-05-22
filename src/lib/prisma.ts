5ccad0f0587822d63dbcdb1412618138b792db9f
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-05-22 11:31:38 -0300

    chore: add note about plugin for database engine

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+// INFO: From fastify docs: https://fastify.dev/docs/v5.2.x/Guides/Database/
+//
+// npm i fastify-plugin
+//
+// 'use strict'
+//
+// import fp from 'fastify-plugin'
+// import { PrismaClient } from "@prisma/client";
+//
+// function prismaPlugin(fastify, options, done) {
+//   if(!fastify.knex) {
+//     const prisma = new PrismaClient(options);
+//     fastify.decorate('prisma', prisma)
+//
+//     fastify.addHook('onClose', (fastify, done) => {
+//       if (fastify.prisma === prisma) {
+//         fastify.prisma.destroy(done)
+//       }
+//     })
+//   }
+//
+//   done()
+// }
+//
+// export default fp(prismaPlugin, { name: 'fastify-knex-example' })
