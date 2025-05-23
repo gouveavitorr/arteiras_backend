29a029682886a94545f08ad628c5362b4d483c5b
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-05-23 16:22:01 -0300

    fix: return deleted object in usecases

--- src/usecases/customer/cart-operations.usecases.ts
+++ src/usecases/customer/cart-operations.usecases.ts
@@ -8,130 +8,129 @@ export interface CartItemRequest {
 
 export const addItemToCart = async (data: CartItemRequest) => {
 
-        const product = await prisma.product.findFirst({
+    const product = await prisma.product.findFirst({
+        where: {
+            id: data.productId
+        }
+    })
+
+    if (!product) {
+        throw new Error("Produto não encontrado.")
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            id: data.userId
+        }
+    })
+
+    if (!user) {
+        throw new Error("Usuário não encontrado.")
+    }
+
+    const cartItemExists = await prisma.cartItem.findFirst({
+        where: {
+            productId: data.productId,
+            userId: data.userId
+        }
+    })
+
+    if (cartItemExists) {
+        const updatedItem = await prisma.cartItem.update({
             where: {
-                id: data.productId
+                id: cartItemExists.id
+            },
+            data: {
+                productId: data?.productId || cartItemExists.productId,
+                userId: data?.userId || cartItemExists.userId,
+                quantity: data.quantity += cartItemExists.quantity
             }
         })
-
-        if (!product) {
-            throw new Error("Produto não encontrado.")
-        }
-
-        const user = await prisma.user.findFirst({
-            where: {
-                id: data.userId
-            }
-        })
-
-        if (!user) {
-            throw new Error("Usuário não encontrado.")
-        }
-
-        const cartItemExists = await prisma.cartItem.findFirst({
-            where: {
+        return updatedItem
+    } else {
+        const item = await prisma.cartItem.create({
+            data: {
                 productId: data.productId,
-                userId: data.userId
+                userId: data.userId,
+                quantity: data.quantity
             }
         })
-
-        if (cartItemExists) {
-            const updatedItem = await prisma.cartItem.update({
-                where: {
-                    id: cartItemExists.id
-                },
-                data: {
-                    productId: data?.productId || cartItemExists.productId,
-                    userId: data?.userId || cartItemExists.userId,
-                    quantity: data.quantity += cartItemExists.quantity
-                }
-            })
-            return updatedItem
-        } else {
-            const item = await prisma.cartItem.create({
-                data: {
-                    productId: data.productId,
-                    userId: data.userId,
-                    quantity: data.quantity
-                }
-            })
-            return item
-        }
+        return item
+    }
 
 }
 
 export const deleteItemFromCart = async (id: string) => {
-
-        const item = await prisma.cartItem.delete({
-            where: {
-                id
-            }
-        })
+    return await prisma.cartItem.delete({
+        where: {
+            id
+        }
+    })
 }
 
-export const removeOneItemFromCart = async(id: string) => {
-
-        const item = await prisma.cartItem.findFirst({
-            where: {
-                id
-            }
-        })
-
-        if (!item) {
-            throw new Error("Produto não encontrado.")
+export const removeOneItemFromCart = async (id: string) => {
+    const item = await prisma.cartItem.findFirst({
+        where: {
+            id
         }
-        if (item.quantity > 0) {
-            item.quantity--
-        } 
+    })
+
+    if (!item) {
+        throw new Error("Produto não encontrado.")
+    }
+
+    if (item.quantity > 0) {
+        item.quantity--
+    }
 }
 
-export const clearCart = async(id: string) => {
+export const clearCart = async (id: string) => {
 
-        const user = await prisma.user.findFirst({
-            where: {
-                id
-            }
-        })
+    const user = await prisma.user.findFirst({
+        where: {
+            id
+        }
+    })
 
-        if(!user) {
-            throw new Error("Cliente não encontrado.")
+    if (!user) {
+        throw new Error("Cliente não encontrado.")
+    }
+    const item = await prisma.cartItem.findMany({
+        where: {
+            userId: user.id
         }
-        const item = await prisma.cartItem.findMany({
-            where: {
-                userId: user.id
-            }
-        })
-        
-        for (let i = 0; i <= (await showCartItems(user.id)).length; i++) { //TODO: pensar um jeito mais eficiente
-            deleteItemFromCart(item[i].id)
-        }
+    })
+
+    for (let i = 0; i <= (await showCartItems(user.id)).length; i++) { //TODO: pensar um jeito mais eficiente
+        deleteItemFromCart(item[i].id)
+    }
 }
 
 export const showCartItems = async (id: string) => {
 
-        const user = await prisma.user.findFirst({
-            where: {
-                id
-            }
-        })
-        const items = await prisma.cartItem.findMany({
-            where: {
-                userId: user?.id
-            }
-        })
-        return items
+    const user = await prisma.user.findFirst({
+        where: {
+            id
+        }
+    })
+    const items = await prisma.cartItem.findMany({
+        where: {
+            userId: user?.id
+        }
+    })
+    return items
 }
 
 export const showUniqueCartItem = async (id: string) => {
 
-        const item = await prisma.cartItem.findFirst({
-            where: {
-                id
-            }
-        })
-        if (!item) {
-            throw new Error("Produto não encontrado.")
+    const item = await prisma.cartItem.findFirst({
+        where: {
+            id
         }
-        return item
+    })
+    if (!item) {
+        throw new Error("Produto não encontrado.")
+    }
+    return item
 
 }
